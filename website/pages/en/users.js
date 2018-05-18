632ee68f5daa12d24f13702d739e8a778020f2e1
Dan Selman <danscode@selman.org>
2018-05-18 17:44:51 +0100

    Add clause logo
    Signed-off-by: Dan Selman <danscode@selman.org>

--- website/pages/en/users.js
+++ website/pages/en/users.js
@@ -32,7 +32,7 @@ class Users extends React.Component {
           <div className="showcaseSection">
             <div className="prose">
               <h1>Who's Using This?</h1>
-              <p>This project is used by many folks</p>
+              <p>This project is used by the following companies</p>
             </div>
             <div className="logos">{showcase}</div>
             <p>Are you using this project?</p>
