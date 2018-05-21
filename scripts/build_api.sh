4b5546535914a27ed41b1f57d1e7b6f19e90eead
Dan Selman <danscode@selman.org>
2018-05-21 15:57:49 +0100

    First cut of API docs
    Signed-off-by: Dan Selman <danscode@selman.org>

---
+++ scripts/build_api.sh
@@ -0,0 +1,11 @@
+npm install -g jsdoc-to-markdown
+
+## Build the Cicero API
+git clone https://github.com/accordproject/cicero.git
+jsdoc2md --template ./scripts/cicero-api.hbs --files ./cicero/packages/cicero-core/index.js ./cicero/packages/cicero-core/lib/*.js > ./docs/cicero-api.md
+rm -rf cicero
+
+## Build the Ergo API
+git clone https://github.com/accordproject/ergo.git
+jsdoc2md --template ./scripts/ergo-api.hbs -c ./scripts/ergo-jsdoc.conf --files ./ergo/packages/**/*.js > ./docs/ergo-api.md
+rm -rf ergo
